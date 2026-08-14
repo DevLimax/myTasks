d9ec7988e46f12a66748e5aefb93eef8c8a6d5fe
João pedro <jpbarroslima12@gmail.com>
2026-08-13 23:22:00 -0300

    feat(user-controller): adicionando a funcionalidade de atualizar a senha

--- src/api/express/controllers/user.controller.ts
+++ src/api/express/controllers/user.controller.ts
@@ -5,6 +5,7 @@ import { UserRepositoryPrisma } from "../../../repositories/user/prisma/user.rep
 import prisma from "../../../repositories/prisma";
 import { UserServiceImplementation } from "../../../services/user/implementation/user.service.implementation";
 import { userUpdateSchema } from "../../../models/schemas/user.schemas";
+import { string } from "zod";
 
 export class UserController {
     private constructor() {};
@@ -63,6 +64,33 @@ export class UserController {
             lastLogin: output?.lastLogin
         }
         res.status(200).json(data).send();
+    }
+
+    public async updatePassword(req: Request, res: Response) {
+        const {id} = req.params;
+        if(typeof id != 'string') return res.status(400).send('Invalid ID');
+
+        const aRepository = UserRepositoryPrisma.build(prisma);
+        const aService = UserServiceImplementation.build(aRepository);
+
+        const validator = userUpdateSchema.safeParse(req.body);
+        if(!validator.success) {
+            const message = validator.error.issues[0]?.message
+            res.status(400).json({message: message}).send();
+            return
+        }
+
+        const {password} = validator.data;
+        if(!password) {
+            res.status(400).send('new password is required')
+            return
+        }
+        try{
+            const user = await aService.updatePassword(id, password);
+            res.status(200).json(user);
+        } catch (e: any) {
+            res.status(500).json({message: e.message});
+        }
     }
 
     public async edit(req: Request, res: Response) {
