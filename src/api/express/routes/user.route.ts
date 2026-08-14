8f90473e08ce806ba7938e10187b32b38e267fdc
João pedro <jpbarroslima12@gmail.com>
2026-08-13 23:09:18 -0300

    feat(user): adicionando rotas de usuario CRUD

---
+++ src/api/express/routes/user.route.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import { UserController } from "../controllers/user.controller";
+import { validateFieldUserCreate } from "../middlewares/user.middleware";
+
+const router = Router();
+const controller = UserController.build();
+
+router.post('/create', validateFieldUserCreate,  controller.save);
+router.get('/', controller.list);
+router.get('/:id', controller.find);
+router.put('/:id', controller.edit);
+router.delete(':id', controller.delete);
+
+export {router as userRouter}
