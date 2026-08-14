48755228d34a2135ca38e4371c52b62e626c65fa
João pedro <jpbarroslima12@gmail.com>
2026-08-13 22:05:05 -0300

    feat(user): Adicionando schemas de validacao dos campos para criar e editar um usuario

---
+++ src/models/schemas/user.schemas.ts
@@ -0,0 +1,25 @@
+import z from "zod";
+
+export const userCreateSchema = z.object({
+    username: z.string({error: 'o campo username deve ser um string'})
+                .min(3, {error: 'username deve ter pelo menos 3 caracteres'})
+                .max(16, {error: 'username deve ter menos de 16 caracteres'}),
+    
+    email: z.email({error: 'Email invalido'}),
+    password: z.string({error: 'a senha deve ser uma string'})
+                .min(6, {error: 'senha deve ter mais de 6 caracteres'})
+                .max(18, {error: 'senha deve ter menos de 18 caracteres'})
+})
+
+export const useUpdateSchema = z.object({
+    username: z.string({error: 'o campo username deve ser um string'})
+                .min(3, {error: 'username deve ter pelo menos 3 caracteres'})
+                .max(16, {error: 'username deve ter menos de 16 caracteres'})
+                .optional(),
+    
+    email: z.email({error: 'Email invalido'}).optional(),
+    password: z.string({error: 'a senha deve ser uma string'})
+                .min(6, {error: 'senha deve ter mais de 6 caracteres'})
+                .max(18, {error: 'senha deve ter menos de 18 caracteres'})
+                .optional()
+})
