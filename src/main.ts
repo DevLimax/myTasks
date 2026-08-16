2231b1ff89dd1418b0a0b38ede0885502fd63409
João pedro <jpbarroslima12@gmail.com>
2026-08-15 23:10:38 -0300

    ss

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(router);
 app.get('/', (req, res) => {
     res.send("Hello World");
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
