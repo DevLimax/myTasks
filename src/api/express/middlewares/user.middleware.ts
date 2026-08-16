9dd673a06a780c5c365ee7a06a8250f29f92b1e4
João pedro <jpbarroslima12@gmail.com>
2026-08-16 14:01:31 -0300

    feat(user-middleware): criando middleware de validacao dos campos de login

--- src/api/express/middlewares/user.middleware.ts
+++ src/api/express/middlewares/user.middleware.ts
@@ -1,8 +1,18 @@
 import type { NextFunction, Request, Response } from "express";
-import { userCreateSchema, userUpdateSchema } from "../../../models/schemas/user.schemas";
+import { userCreateSchema, userLoginSchema, userUpdateSchema } from "../../../models/schemas/user.schemas";
 
 export async function validateFieldUserCreate(req: Request, res: Response, next: NextFunction) {
     const result = userCreateSchema.safeParse(req.body);
+    if(!result.success) {
+        const message = result.error.issues[0]?.message
+        res.status(400).json({message: message}).send();
+        return
+    }
+    next();
+}
+
+export async function validateLoginCredentials(req: Request, res: Response, next: NextFunction) {
+    const result = userLoginSchema.safeParse(req.body);
     if(!result.success) {
         const message = result.error.issues[0]?.message
         res.status(400).json({message: message}).send();
