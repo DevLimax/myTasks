3968bb7faf24fb47b2f5322cf3f7861fac76ca66
João pedro <jpbarroslima12@gmail.com>
2026-08-16 23:52:54 -0300

    feat(api-router): adicionado as rotas da entidade (task) nas rotas principais

--- src/api/express/api.ts
+++ src/api/express/api.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRouter } from "./routes/user.route";
+import { taskRouter } from "./routes/task.route";
 
 const router = Router();
 
 router.use('/users', userRouter);
+router.use('/tasks', taskRouter);
 
 export default router
