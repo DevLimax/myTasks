9231f9069bb6565c3bdd2ef98bc19c00644859c1
João pedro <jpbarroslima12@gmail.com>
2026-08-16 23:51:28 -0300

    feat(task): adicionado o modulo de controller da entidade (task)

---
+++ src/api/express/controllers/task.controller.ts
@@ -0,0 +1,71 @@
+import type { Request, Response } from "express";
+import type { TaskInputDto, TaskService, TaskUpdateInputDto } from "../../../services/task/task.service";
+
+
+export class TaskController {
+    private constructor(private service: TaskService) {}
+
+    public static build(service: TaskService) {
+        return new TaskController(service);
+    }
+
+    public save = async (req: Request, res: Response) => {
+        const {userId, title, description, status, priority} = req.body;
+        const data: TaskInputDto = {
+            userId,
+            title,
+            description,
+            status,
+            priority
+        };
+        try{
+            const newTask = await this.service.save(data);
+            res.status(201).json(newTask);
+        } catch(e: any) {
+            res.status(400).json({message: e.message});
+        }
+    }
+
+    public list = async (req: Request, res: Response) => {
+        // const {userId, status, priority} = req.params; -> futuros campos de filter
+        const tasks = await this.service.list();
+        res.status(200).json(tasks);
+    }
+
+    public find = async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const task = await this.service.find(id);
+        if(!task) {
+            res.status(404).send('task not found');
+        } else {
+            res.status(200).json(task);
+        }
+    }
+
+    public update = async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const {title, description, status, priority} = req.body;
+        const data: TaskUpdateInputDto = {
+            title,
+            description,
+            status,
+            priority
+        }
+        try{
+            const taskUpdated = await this.service.update(id, data);
+            res.status(200).json(taskUpdated);
+        } catch(e: any) {
+            res.status(400).json({message: e.message});
+        }
+    }
+
+    public delete = async (req: Request, res: Response) => {
+        const {id} = req.params;
+        try{
+            await this.service.delete(id);
+            res.status(204).send();
+        } catch(e: any) {
+            res.status(400).json({message: e.message});
+        }
+    }
+}
