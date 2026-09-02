5eb0810e107580ee7ab3720d3d5c0f6ca9c6c0ab
João pedro <jpbarroslima12@gmail.com>
2026-09-02 20:58:28 -0300

    feat(refactor-router): alterando endpoints de edicao de usuario e redefinicao de senha

--- src/api/express/routes/user.route.ts
+++ src/api/express/routes/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { UserController } from "../controllers/user.controller";
-import { validateFieldUserCreate } from "../middlewares/user.middleware";
+import { checkTokenValid, validateFieldUserCreate } from "../middlewares/user.middleware";
 
 const router = Router();
 const controller = UserController.build();
@@ -10,8 +10,8 @@ router.post('/create', validateFieldUserCreate,  controller.save);
 router.post('/login', controller.login);
 router.get('/', controller.list);
 router.get('/:id', controller.find);
-router.put('/:id', controller.edit);
-router.put('/updatePassword/:id', controller.updatePassword);
+router.put('/', checkTokenValid, controller.edit);
+router.put('/updatePassword/', checkTokenValid, controller.updatePassword);
 router.delete('/:id', controller.delete);
 
 export {router as userRouter}
