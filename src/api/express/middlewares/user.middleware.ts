3707c0a95a3e97730fba8a6ae087524daea7ed64
João pedro <jpbarroslima12@gmail.com>
2026-09-02 20:57:16 -0300

    feat(user-middleware): adicionando middleware de validacao do token na request do endpoint

--- src/api/express/middlewares/user.middleware.ts
+++ src/api/express/middlewares/user.middleware.ts
@@ -1,5 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
-import { userCreateSchema, userLoginSchema, userUpdateSchema } from "../../../models/schemas/user.schemas";
+import { userCreateSchema, userLoginSchema } from "../../../models/schemas/user.schemas";
+import prisma from "../../../repositories/prisma";
+import { UserRepositoryPrisma } from "../../../repositories/user/prisma/user.repository.prisma";
+import { exportPayload, isTokenExpired } from "../../../utils/token.utils";
+import type { JwtPayload } from "jsonwebtoken";
 
 export async function validateFieldUserCreate(req: Request, res: Response, next: NextFunction) {
     const result = userCreateSchema.safeParse(req.body);
@@ -16,6 +20,26 @@ export async function validateLoginCredentials(req: Request, res: Response, next
     if(!result.success) {
         const message = result.error.issues[0]?.message
         res.status(400).json({message: message}).send();
+        return
+    }
+    next();
+}
+
+export async function checkTokenValid(req: Request, res: Response, next: NextFunction) {
+    const repository = UserRepositoryPrisma.build(prisma);
+    const authToken = req.headers['authorization']?.split(' ')[1];
+    if(!authToken) {
+        res.status(401).send("you need to log in to perform this action.")
+        return
+    }
+    const payload: JwtPayload = exportPayload(authToken);
+    const tokenExpired = isTokenExpired(payload.exp);
+    if(tokenExpired) {
+        res.status(401).send("you need to log in to perform this action.")
+    }
+    const result = await repository.find(payload['id']);
+    if(!result) {
+        res.status(401).send("it is not possible to perform this action.")
         return
     }
     next();
