429cf0792079a59c54e5c7d1b3ce45f26038f3d0
João pedro <jpbarroslima12@gmail.com>
2026-09-02 20:59:17 -0300

    feat(utils): criando uma util para gerar e validar tokens JWT

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -1,5 +1,5 @@
 import "dotenv/config"
-import jwt from "jsonwebtoken"
+import jwt, { type JwtPayload } from "jsonwebtoken"
 
 interface UserPayload {
     id: string,
@@ -18,3 +18,13 @@ export function generateRefreshToken(payload: UserPayload): string {
     const token = jwt.sign(payload, secret, {expiresIn: '1d'});
     return token
 };
+
+export function exportPayload(token: string): JwtPayload | string {
+    const payload = jwt.verify(token, secret);
+    return payload
+}
+
+export function isTokenExpired(exp: number): boolean {
+    const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+    return currentTimeInSeconds > exp;
+}
