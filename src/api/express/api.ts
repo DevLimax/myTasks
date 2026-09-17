3451064ff9f9d2063b64291bea73b144f158222d
João pedro <jpbarroslima12@gmail.com>
2026-09-17 00:26:50 -0300

    feat(api): adicionando o middleware de validacao do usuario para acesso aos endpoints de task

--- src/api/express/api.ts
+++ src/api/express/api.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { userRouter } from "./routes/user.route";
 import { taskRouter } from "./routes/task.route";
+import { checkTokenValid } from "./middlewares/user.middleware";
 
 const router = Router();
 
 router.use('/users', userRouter);
-router.use('/tasks', taskRouter);
+router.use('/tasks', checkTokenValid, taskRouter);
 
 export default router
