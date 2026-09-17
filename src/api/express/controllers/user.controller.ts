ce815c0ee5562e3152849e994472c23954d61c60
João pedro <jpbarroslima12@gmail.com>
2026-09-16 21:47:14 -0300

    refactory(user-controller): refatorando o codigo de usuarios para o metodo de arrow function

--- src/api/express/controllers/user.controller.ts
+++ src/api/express/controllers/user.controller.ts
@@ -1,28 +1,20 @@
 import type { Request, Response } from "express";
 
-import { UserRepositoryPrisma } from "../../../repositories/user/prisma/user.repository.prisma";
-
-import prisma from "../../../repositories/prisma";
-import { UserServiceImplementation } from "../../../services/user/implementation/user.service.implementation";
 import { userUpdateSchema } from "../../../models/schemas/user.schemas";
 import { exportPayload } from "../../../utils/token.utils";
-import type { JwtPayload } from "jsonwebtoken";
+import type { UserService } from "../../../services/user/user.service";
 
 export class UserController {
-    private constructor() {};
+    private constructor(private service: UserService) {};
 
-    public static build() {
-        return new UserController();
+    public static build(service: UserService) {
+        return new UserController(service);
     };
 
-    public async save(req: Request, res: Response) {
+    public save = async (req: Request, res: Response) => {
         const {username, email, password} = req.body;
-
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
         try {
-            const output = await aService.save(username, email, password);
+            const output = await this.service.save(username, email, password);
             const data = {
                 id: output.id,
                 username,
@@ -35,24 +27,17 @@ export class UserController {
         }
     }
 
-    public async list(req: Request, res: Response) {
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
-        const output = await aService.list();
+    public list = async (req: Request, res: Response) => {
+        const output = await this.service.list();
         const data = {
             users: output.users
         };
         res.status(200).json(data).send();
     }
 
-    public async find(req: Request, res: Response) {
+    public find = async (req: Request, res: Response) => {
         const {id} = req.params;
-
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
-        const output = typeof id === 'string' ? await aService.find(id) : null;
+        const output = typeof id === 'string' ? await this.service.find(id) : null;
         if(!output) {
             res.status(404).send(`user not found`);
             return
@@ -67,13 +52,10 @@ export class UserController {
         res.status(200).json(data).send();
     }
 
-    public async updatePassword(req: Request, res: Response) {
+    public updatePassword = async (req: Request, res: Response) => {
         const {id}: any = exportPayload(req.headers['authorization']?.split(' ')[1] || '');
         if(typeof id != 'string') return res.status(400).send('Invalid ID');
-
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
+        
         const validator = userUpdateSchema.safeParse(req.body);
         if(!validator.success) {
             const message = validator.error.issues[0]?.message
@@ -87,19 +69,16 @@ export class UserController {
             return
         }
         try{
-            const user = await aService.updatePassword(id, password);
+            const user = await this.service.updatePassword(id, password);
             res.status(200).json(user);
         } catch (e: any) {
             res.status(500).json({message: e.message});
         }
     }
 
-    public async edit(req: Request, res: Response) {
+    public edit = async (req: Request, res: Response) => {
         const {id}: any = exportPayload(req.headers['authorization']?.split(' ')[1] || '');
         if(typeof id != 'string') return res.status(400).send('Invalid ID');
-
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
 
         const validator = userUpdateSchema.safeParse(req.body);
         if(!validator.success) {
@@ -110,24 +89,21 @@ export class UserController {
 
         const {username, email} = validator.data;
         try {
-            const user = await aService.update(id, {newUsername: username, newEmail: email});
+            const user = await this.service.update(id, {newUsername: username, newEmail: email});
             res.status(200).json(user).send();
         } catch (e: any) {
             res.status(500).json({message: e.message});
         };
     }
 
-    public async delete(req: Request, res: Response) {
+    public delete = async (req: Request, res: Response) => {
         const {id} = req.params;
         if(typeof id != 'string') {
             res.status(400).send('invalid ID')
             return
         }
 
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
-        const aUser = await aService.find(id);
+        const aUser = await this.service.find(id);
 
         if(!aUser) {
             res.status(404).send("user not found");
@@ -135,21 +111,17 @@ export class UserController {
         }
 
         try{
-            await aService.delete(id);
+            await this.service.delete(id);
             res.status(204).send();
         } catch(e: any) {
             res.status(400).json({message: e.message});
         }
     }
 
-    public async login(req: Request, res: Response) {
+    public login = async (req: Request, res: Response) => {
         const {email, password} = req.body;
-
-        const aRepository = UserRepositoryPrisma.build(prisma);
-        const aService = UserServiceImplementation.build(aRepository);
-
         try{
-            const output = await aService.login(email, password);
+            const output = await this.service.login(email, password);
             res.status(201).json(output);
         } catch(e: any) {
             res.status(400).json({message: e.message});
